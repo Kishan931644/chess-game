1d9ab027b1e442a11b814a508b6566576e9d317b
kishan93273110 <techno931644@gmail.com>
2022-08-29 21:12:29 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const whitePieces = document.querySelectorAll(".white-piece");
+    const blackPieces = document.querySelectorAll(".black-piece");
+
+    
+    document.querySelectorAll(".soldier").forEach(soldier => {
+        soldier.addEventListener("click", () => {
+             
+        });
+    });
+
+});
