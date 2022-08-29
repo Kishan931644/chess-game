996f3c4ca6d80014187c2e6354361c0070fedd38
kishan93273110 <techno931644@gmail.com>
2022-08-29 22:26:18 +0530

    pawn moves complete

--- script.js
+++ script.js
@@ -1,24 +1,70 @@
+const whitePieces = document.querySelectorAll(".white-piece");
+const blackPieces = document.querySelectorAll(".black-piece");
+let currentSoldier;
+let posibleMoves =[];
+
 document.addEventListener("DOMContentLoaded", () => {
-    const whitePieces = document.querySelectorAll(".white-piece");
-    const blackPieces = document.querySelectorAll(".black-piece");
 
-    
     document.querySelectorAll(".soldier").forEach(soldier => {
-        soldier.addEventListener("click", ({target}) => {
+        soldier.addEventListener("click", ({ target }) => {
+            removeMoves();
+            currentSoldier = target;
+
+            let currentRow = target.closest(".row");
+            let currentCol = target.closest(".cell");
+            let currentPos = currentRow.dataset["row"] + currentCol.dataset["col"];
+
             if (target.classList.contains("pawn")) {
-
-                let currentRow = target.closest(".row");
-                let currentCol = target.closest(".cell");
-                let currentPos = currentRow.dataset["row"] + currentCol.dataset["col"];
-                console.log(currentPos);
-
+                let move;
                 if (target.classList.contains("white-piece")) {
-                    
+                    move = parseInt(currentPos) - 10;
                 } else if (target.classList.contains("black-piece")) {
-                    
+                    move = parseInt(currentPos) + 10;
                 }
+
+                document.querySelector("#row-" + parseInt(move / 10) + " .col-" + parseInt(move % 10)).classList.add("posibleMove");
+            } else if (target.classList.contains("castle")) {
+                let nextMove = parseInt(currentPos) - 10;
+
+                while (document.querySelector("#row-" + parseInt(nextMove / 10) + " .col-" + parseInt(nextMove % 10)).hasChildNodes() == false) {
+                    posibleMoves.push(document.querySelector("#row-" + parseInt(nextMove / 10) + " .col-" + parseInt(nextMove % 10)));
+                }
+
+                console.log(posibleMoves);
+                addMoves();
             }
+            console.log("hii");
+            MoveSoldier();
         });
     });
 
+
+    document.addEventListener("click", ({ target }) => {
+        if (!target.classList.contains("soldier"))
+            removeMoves();
+    });
+
 });
+
+function addMoves() {
+    posibleMoves.forEach(element => {
+        element.classList.add("posibleMove");
+    });
+}
+
+function removeMoves() {
+    document.querySelectorAll(".posibleMove").forEach(cell => {
+        cell.removeAttribute("onclick");
+        cell.classList.remove("posibleMove");
+    });
+}
+
+function MoveSoldier() {
+    document.querySelectorAll(".posibleMove").forEach(cell => {
+        cell.setAttribute("onclick","move(event)");
+    });
+}
+
+function move({target}) {
+    target.append(currentSoldier);
+}
