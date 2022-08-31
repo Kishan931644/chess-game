0cb77c7609fdf6fb5634c590e30568e5be0d0ae5
kishan93273110 <techno931644@gmail.com>
2022-08-31 17:44:11 +0530

    chess is complete

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
-const whitePieces = document.querySelectorAll(".white-piece");
-const blackPieces = document.querySelectorAll(".black-piece");
+const piece = [
+    ".white-piece",
+    ".black-piece"
+];
+
+let moveAudio = new Audio("movesound.mp3");
+let moveAble = 0;
 let currentSoldier;
 let posibleMoves = [];
 
@@ -16,85 +21,102 @@ document.addEventListener("DOMContentLoaded", () => {
             if (target.classList.contains("pawn")) {
                 let move;
                 if (target.classList.contains("white-piece")) {
-                    move = currentPos - 10;
+                    if (document.querySelector("#row-" + (parseInt(currentPos / 10) - 1) + " .col-" + (parseInt(currentPos % 10) - 1)) != null && document.querySelector("#row-" + (parseInt(currentPos / 10) - 1) + " .col-" + (parseInt(currentPos % 10) - 1)).hasChildNodes() != false)
+                        pushElementInArray(target.classList[1], (parseInt(currentPos / 10) - 1), (parseInt(currentPos % 10) - 1));
+
+                    if (document.querySelector("#row-" + (parseInt(currentPos / 10) - 1) + " .col-" + (parseInt(currentPos % 10) + 1)) != null && document.querySelector("#row-" + (parseInt(currentPos / 10) - 1) + " .col-" + (parseInt(currentPos % 10) + 1)).hasChildNodes() != false)
+                        pushElementInArray(target.classList[1], (parseInt(currentPos / 10) - 1), (parseInt(currentPos % 10) + 1));
+
+                    if (document.querySelector("#row-" + (parseInt(currentPos / 10) - 1) + " .col-" + (parseInt(currentPos % 10))).hasChildNodes() != true) {
+                        move = currentPos - 10;
+                        pushElementInArray(target.classList[1], move / 10, move % 10);
+                    }
+
                 } else if (target.classList.contains("black-piece")) {
-                    move = currentPos + 10;
+                    if (document.querySelector("#row-" + (parseInt(currentPos / 10) + 1) + " .col-" + (parseInt(currentPos % 10) - 1)) != null && document.querySelector("#row-" + (parseInt(currentPos / 10) + 1) + " .col-" + (parseInt(currentPos % 10) - 1)).hasChildNodes() != false)
+                        pushElementInArray(target.classList[1], (parseInt(currentPos / 10) + 1), (parseInt(currentPos % 10) - 1));
+                    if (document.querySelector("#row-" + (parseInt(currentPos / 10) + 1) + " .col-" + (parseInt(currentPos % 10) + 1)) != null && document.querySelector("#row-" + (parseInt(currentPos / 10) + 1) + " .col-" + (parseInt(currentPos % 10) + 1)).hasChildNodes() != false)
+                        pushElementInArray(target.classList[1], (parseInt(currentPos / 10) + 1), (parseInt(currentPos % 10) + 1));
+
+                    if (document.querySelector("#row-" + (parseInt(currentPos / 10) + 1) + " .col-" + (parseInt(currentPos % 10))).hasChildNodes() != true) {
+                        move = currentPos + 10;
+                        pushElementInArray(target.classList[1], move / 10, move % 10);
+                    }
                 }
 
-                pushElementInArray(move / 10, move % 10);
             } else if (target.classList.contains("castle") || target.classList.contains("queen")) {
                 for (let rowUp = parseInt(currentRow.dataset["row"]) - 1; rowUp >= 1; rowUp--) {
-                    if (pushElementInArray(rowUp, currentCol.dataset["col"]) == false) {
+                    if (pushElementInArray(target.classList[1], rowUp, currentCol.dataset["col"]) == false) {
                         break;
                     }
                 }
 
                 for (let rowDown = parseInt(currentRow.dataset["row"]) + 1; rowDown <= 8; rowDown++) {
-                    if (pushElementInArray(rowDown, currentCol.dataset["col"]) == false) {
+                    if (pushElementInArray(target.classList[1], rowDown, currentCol.dataset["col"]) == false) {
                         break;
                     }
                 }
                 for (let colLeft = parseInt(currentCol.dataset["col"]) - 1; colLeft >= 1; colLeft--) {
-                    if (pushElementInArray(currentRow.dataset["row"], colLeft) == false) {
+                    if (pushElementInArray(target.classList[1], currentRow.dataset["row"], colLeft) == false) {
                         break;
                     }
                 }
 
                 for (let colRight = parseInt(currentCol.dataset["col"]) + 1; colRight <= 8; colRight++) {
-                    if (pushElementInArray(currentRow.dataset["row"], colRight) == false) {
+                    if (pushElementInArray(target.classList[1], currentRow.dataset["row"], colRight) == false) {
                         break;
                     }
                 }
 
             } else if (target.classList.contains("knight")) {
                 // Down right left
-                pushElementInArray(parseInt(currentRow.dataset["row"]) + 2, parseInt(currentCol.dataset["col"]) + 1);
-                pushElementInArray(parseInt(currentRow.dataset["row"]) + 2, parseInt(currentCol.dataset["col"]) - 1);
+                pushElementInArray(target.classList[1], parseInt(currentRow.dataset["row"]) + 2, parseInt(currentCol.dataset["col"]) + 1);
+                pushElementInArray(target.classList[1], parseInt(currentRow.dataset["row"]) + 2, parseInt(currentCol.dataset["col"]) - 1);
 
                 // Up right left
-                pushElementInArray(parseInt(currentRow.dataset["row"]) - 2, parseInt(currentCol.dataset["col"]) - 1);
-                pushElementInArray(parseInt(currentRow.dataset["row"]) - 2, parseInt(currentCol.dataset["col"]) + 1);
+                pushElementInArray(target.classList[1], parseInt(currentRow.dataset["row"]) - 2, parseInt(currentCol.dataset["col"]) - 1);
+                pushElementInArray(target.classList[1], parseInt(currentRow.dataset["row"]) - 2, parseInt(currentCol.dataset["col"]) + 1);
 
                 // right up Down
-                pushElementInArray(parseInt(currentRow.dataset["row"]) + 1, parseInt(currentCol.dataset["col"]) + 2);
-                pushElementInArray(parseInt(currentRow.dataset["row"]) - 1, parseInt(currentCol.dataset["col"]) + 2);
+                pushElementInArray(target.classList[1], parseInt(currentRow.dataset["row"]) + 1, parseInt(currentCol.dataset["col"]) + 2);
+                pushElementInArray(target.classList[1], parseInt(currentRow.dataset["row"]) - 1, parseInt(currentCol.dataset["col"]) + 2);
 
                 // left up down
-                pushElementInArray(parseInt(currentRow.dataset["row"]) + 1, parseInt(currentCol.dataset["col"]) - 2);
-                pushElementInArray(parseInt(currentRow.dataset["row"]) - 1, parseInt(currentCol.dataset["col"]) - 2);
+                pushElementInArray(target.classList[1], parseInt(currentRow.dataset["row"]) + 1, parseInt(currentCol.dataset["col"]) - 2);
+                pushElementInArray(target.classList[1], parseInt(currentRow.dataset["row"]) - 1, parseInt(currentCol.dataset["col"]) - 2);
             } else if (target.classList.contains("king")) {
-                pushElementInArray(parseInt(currentRow.dataset["row"]) + 1, parseInt(currentCol.dataset["col"]));
-                pushElementInArray(parseInt(currentRow.dataset["row"]) + 1, parseInt(currentCol.dataset["col"]) + 1);
+                pushElementInArray(target.classList[1], parseInt(currentRow.dataset["row"]) + 1, parseInt(currentCol.dataset["col"]));
+                pushElementInArray(target.classList[1], parseInt(currentRow.dataset["row"]) + 1, parseInt(currentCol.dataset["col"]) + 1);
 
-                pushElementInArray(parseInt(currentRow.dataset["row"]) - 1, parseInt(currentCol.dataset["col"]));
-                pushElementInArray(parseInt(currentRow.dataset["row"]) - 1, parseInt(currentCol.dataset["col"]) - 1);
+                pushElementInArray(target.classList[1], parseInt(currentRow.dataset["row"]) - 1, parseInt(currentCol.dataset["col"]));
+                pushElementInArray(target.classList[1], parseInt(currentRow.dataset["row"]) - 1, parseInt(currentCol.dataset["col"]) - 1);
 
-                pushElementInArray(parseInt(currentRow.dataset["row"]), parseInt(currentCol.dataset["col"]) + 1);
-                pushElementInArray(parseInt(currentRow.dataset["row"]) - 1, parseInt(currentCol.dataset["col"]) + 1);
+                pushElementInArray(target.classList[1], parseInt(currentRow.dataset["row"]), parseInt(currentCol.dataset["col"]) + 1);
+                pushElementInArray(target.classList[1], parseInt(currentRow.dataset["row"]) - 1, parseInt(currentCol.dataset["col"]) + 1);
 
-                pushElementInArray(parseInt(currentRow.dataset["row"]), parseInt(currentCol.dataset["col"]) - 1);
-                pushElementInArray(parseInt(currentRow.dataset["row"]) + 1, parseInt(currentCol.dataset["col"]) - 1);
+                pushElementInArray(target.classList[1], parseInt(currentRow.dataset["row"]), parseInt(currentCol.dataset["col"]) - 1);
+                pushElementInArray(target.classList[1], parseInt(currentRow.dataset["row"]) + 1, parseInt(currentCol.dataset["col"]) - 1);
 
             }
 
             if (target.classList.contains("bishop") || target.classList.contains("queen")) {
                 for (let upRight = parseInt(currentRow.dataset["row"]) - 1, j = 1; upRight >= 1, j <= 8; upRight--, j++) {
                     var colNumber = parseInt(currentCol.dataset["col"]) + j;
-                    if (pushElementInArray(upRight, colNumber) == false) {
+                    if (pushElementInArray(target.classList[1], upRight, colNumber) == false) {
                         break;
                     }
                 }
 
                 for (let downLeft = parseInt(currentRow.dataset["row"]) + 1, j = 1; downLeft <= 8, j <= 8; downLeft++, j++) {
                     var colNumber = parseInt(currentCol.dataset["col"]) - j;
-                    if (pushElementInArray(downLeft, colNumber) == false) {
+                    if (pushElementInArray(target.classList[1], downLeft, colNumber) == false) {
                         break;
                     }
                 }
 
                 for (let upLeft = parseInt(currentCol.dataset["col"]) - 1, j = 1; upLeft >= 1, j <= 8; upLeft--, j++) {
                     var rowNumber = parseInt(currentRow.dataset["row"]) - j;
-                    if (pushElementInArray(rowNumber, upLeft) == false) {
+                    if (pushElementInArray(target.classList[1], rowNumber, upLeft) == false) {
                         break;
                     }
                 }
@@ -102,7 +124,7 @@ document.addEventListener("DOMContentLoaded", () => {
                 for (let downRight = parseInt(currentCol.dataset["col"]) + 1, j = 1; downRight <= 8, j <= 8; downRight++, j++) {
                     var rowNumber = parseInt(currentRow.dataset["row"]) + j;
 
-                    if (pushElementInArray(rowNumber, downRight) == false) {
+                    if (pushElementInArray(target.classList[1], rowNumber, downRight) == false) {
                         break;
                     }
                 }
@@ -113,22 +135,24 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     });
 
-
     document.addEventListener("click", ({ target }) => {
         if (!target.classList.contains("soldier"))
             removeMoves();
     });
-
 });
 
-function pushElementInArray(rowNumber, colNumber) {
+function pushElementInArray(type, rowNumber, colNumber) {
     colNumber = parseInt(colNumber);
     rowNumber = parseInt(rowNumber);
     var selectedEle = document.querySelector("#row-" + rowNumber + " .col-" + colNumber)
-
-    if (selectedEle != null && selectedEle.hasChildNodes() == false) {
+    if (selectedEle == null) {
+        return false
+    } else if (selectedEle.hasChildNodes() == false) {
         posibleMoves.push(selectedEle);
         return true;
+    } else if (selectedEle.hasChildNodes() != false && selectedEle.children[0].classList[1] != type) {
+        posibleMoves.push(selectedEle);
+        return false;
     } else {
         return false;
     }
@@ -145,6 +169,8 @@ function removeMoves() {
         cell.removeAttribute("onclick");
         cell.classList.remove("posibleMove");
     });
+
+
     posibleMoves = [];
 }
 
@@ -155,5 +181,15 @@ function MoveSoldier() {
 }
 
 function move({ target }) {
+    if (target.hasChildNodes() == true)
+        target.children[0].remove();
+
     target.append(currentSoldier);
+
+    document.querySelectorAll(".soldier").forEach(soldier => {
+        soldier.classList.toggle("not-movable");
+        soldier.classList.toggle("is-move");
+    });
+
+    moveAudio.play();
 }
